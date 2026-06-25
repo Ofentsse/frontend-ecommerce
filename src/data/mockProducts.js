3ffa4361f64a639068260430601c34a0cfc9fc89
OfentsseDev <info.ofentsemosala@gmail.com>
2026-06-25 11:47:54 +0200

    mock product catalog with categories and reviews

---
+++ src/data/mockProducts.js
@@ -0,0 +1,176 @@
+export const newArrivals = [
+  {
+    id: 1,
+    name: "T-shirt with Tape Details",
+    price: 120,
+    rating: 4.5,
+    image: "https://i.pinimg.com/736x/26/2a/e3/262ae33fd2c29704606c3037aa64f39f.jpg"
+  },
+  {
+    id: 2,
+    name: "Skinny Fit Jeans",
+    price: 240,
+    originalPrice: 260,
+    discount: 20,
+    rating: 3.5,
+    image: "https://i.pinimg.com/736x/f8/c2/34/f8c234049462a71b56ef4564db84c0f4.jpg"
+  },
+  {
+    id: 3,
+    name: "Checkered Shirt",
+    price: 180,
+    rating: 4.5,
+    image: "https://i.pinimg.com/1200x/40/6c/83/406c8377427fca81fec05fc612091472.jpg"
+  },
+  {
+    id: 4,
+    name: "Sleeve Striped T-shirt",
+    price: 130,
+    originalPrice: 160,
+    discount: 30,
+    rating: 4.5,
+    image: "https://i.pinimg.com/736x/b4/7d/d2/b47dd2fa8aa2ea444ed9df049f5dde15.jpg"
+  }
+];
+
+export const topSelling = [
+  {
+    id: 5,
+    name: "Windstopper Shell",
+    price: 212,
+    originalPrice: 232,
+    discount: 20,
+    rating: 5.0,
+    image: "https://i.pinimg.com/736x/56/cd/0f/56cd0fc8f6e13657eb1c04b5466c7e49.jpg"
+  },
+  {
+    id: 6,
+    name: "Courage Graphic T-shirt",
+    price: 145,
+    rating: 4.0,
+    image: "https://i.pinimg.com/736x/8d/38/9d/8d389d1f480c4dc23070c2499e185db7.jpg"
+  },
+  {
+    id: 7,
+    name: "Loose Fit Bermuda Shorts",
+    price: 80,
+    rating: 3.5,
+    image: "https://i.pinimg.com/736x/ef/b0/fb/efb0fb558d153c9e984ce6810454b68d.jpg"
+  },
+  {
+    id: 8,
+    name: "Vintage 90s Nautica Competition ",
+    price: 210,
+    rating: 4.5,
+    image: "https://i.pinimg.com/1200x/a4/de/9b/a4de9be07db4979aadfb42e9e873d889.jpg"
+  }
+];
+
+export const youMightAlsoLike = [
+  {
+    id: 9,
+    name: "Polo with Contrast Trims",
+    price: 212,
+    originalPrice: 242,
+    discount: 20,
+    rating: 4.0,
+    image: "https://images.unsplash.com/photo-1586363104862-3a5e2ab60d99?w=500&q=80"
+  },
+  {
+    id: 10,
+    name: "Gradient Graphic T-shirt",
+    price: 145,
+    rating: 3.5,
+    image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=500&q=80"
+  },
+  {
+    id: 11,
+    name: "Polo with Tipping Details",
+    price: 180,
+    rating: 4.5,
+    image: "https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?w=500&q=80"
+  },
+  {
+    id: 12,
+    name: "Black Striped T-shirt",
+    price: 120,
+    originalPrice: 150,
+    discount: 30,
+    rating: 5.0,
+    image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=500&q=80"
+  }
+];
+
+export const categoryProducts = [
+  {
+    id: 13,
+    name: "Gradient Graphic T-shirt",
+    price: 145,
+    rating: 3.5,
+    image: "https://i.pinimg.com/736x/1d/90/6d/1d906d7a8a059cba098a475b1a246be9.jpg"
+  },
+  {
+    id: 14,
+    name: "Polo with Tipping Details",
+    price: 180,
+    rating: 4.5,
+    image: "https://i.pinimg.com/736x/65/a7/1b/65a71be706ce6d31e8a3b1277a86b205.jpg"
+  },
+  {
+    id: 15,
+    name: "Black Striped T-shirt",
+    price: 120,
+    originalPrice: 150,
+    discount: 30,
+    rating: 5.0,
+    image: "https://i.pinimg.com/736x/82/63/e1/8263e1538c929b8ea6df1dcbceda52ba.jpg"
+  },
+  {
+    id: 16,
+    name: "Skinny Fit Jeans",
+    price: 240,
+    originalPrice: 260,
+    discount: 20,
+    rating: 3.5,
+    image: "https://i.pinimg.com/1200x/86/f7/42/86f742c0ae7760940152bbdfed8804d7.jpg"
+  },
+  {
+    id: 17,
+    name: "Checkered Shirt",
+    price: 180,
+    rating: 4.5,
+    image: "https://i.pinimg.com/736x/7d/42/01/7d42019235481a160ee44c70ebf901fb.jpg"
+  },
+  {
+    id: 18,
+    name: "Sleeve Striped T-shirt",
+    price: 130,
+    originalPrice: 160,
+    discount: 30,
+    rating: 4.5,
+    image: "https://i.pinimg.com/736x/3a/4f/6a/3a4f6a0951cea5980ddb74d4d097974e.jpg"
+  },
+  {
+    id: 19,
+    name: "Vertical Striped Shirt",
+    price: 212,
+    originalPrice: 232,
+    discount: 20,
+    rating: 5.0,
+    image: "https://i.pinimg.com/1200x/df/c0/31/dfc031674df70298e4ca79fa60e224b4.jpg"
+  },
+  {
+    id: 20,
+    name: "Courage Graphic T-shirt",
+    price: 145,
+    rating: 4.0,
+    image: "https://i.pinimg.com/1200x/37/c6/a4/37c6a42613f662a4463b35f7e76a0d4a.jpg"
+  },
+  {
+    id: 21,
+    name: "Loose Fit Bermuda Shorts",
+    price: 80,
+    rating: 3.5,
+    image: "https://i.pinimg.com/1200x/4a/07/2e/4a072e08a4e11bfd96b12cc136a4732a.jpg"
+  }
+];
